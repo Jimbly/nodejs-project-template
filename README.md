1bb2160a013b5ad3f919594094fedb4a98ad6a28
Jimb Esser <wasteland@gmail.com>
2015-12-09 11:00:02 -0800

    Cleanup

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 Template for a state of the art Javascript project
 ==================================================
 
+* Files can be ES6 (through Babel) or TypeScript.
+* Server automatically restarts on any relevant file change
+* Client automatically reloads on javascript or html change
+* Client automatically dynamically reloads CSS file changes
+
 Used SublimeText 3 packages (if using TypeScript):
 * ArcticTypescript
 
@@ -13,10 +18,9 @@ tsd query node --action install --save
 * to update: `tsd reinstall --save`
 
 TODO:
-* ts and js tasks are exiting immediately despite not being done?
-* lint errors to console on save/reload? TypeScript just fails to compile, so maybe that's fine for TS files
-* test results to console on save? separate task for this?
-* server should not restart on save of client file
+* test results to console on save?
+* minify, bundle CSS
+* bundle vendor .js files?
 
 References used:
 * https://github.com/JacksonGariety/gulp-nodemon/issues/85
--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,11 @@ var browserify = require('browserify');
 var browser_sync = require('browser-sync');
 var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
-var gulp = require('gulp');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 var node_inspector = require('gulp-node-inspector');
 var nodemon = require('gulp-nodemon');
 var source = require('vinyl-source-stream');
-var sourcemaps = require('gulp-sourcemaps');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 var tsify = require('tsify');
@@ -22,6 +21,7 @@ var watchify = require('watchify');
 var config = {
   ts_files: ['src/**/*.ts', '!src/client/**/*.ts'],
   js_files: ['src/**/*.js', '!src/client/**/*.js'],
+  all_js_files: ['src/**/*.js'],
   client_html: ['src/client/**/*.html'],
   client_css: ['src/client/**/*.css'],
 };
@@ -40,7 +40,7 @@ gulp.task('ts', function() {
   var ts_project = ts.createProject('tsconfig.json', {
     sortOutput: true,
   });
-  ts_project.src()
+  return ts_project.src()
     .pipe(sourcemaps.init())
     .pipe(ts(ts_project))
     .js
@@ -49,28 +49,36 @@ gulp.task('ts', function() {
 });
 
 gulp.task('js', function () {
-  gulp.src(config.js_files)
+  return gulp.src(config.js_files)
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build'));
 });
 
+gulp.task('jshint', function () {
+  return gulp.src(config.all_js_files)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
 //////////////////////////////////////////////////////////////////////////
 // client tasks
+gulp.task('client_html', function () {
+  return gulp.src(config.client_html)
+    .pipe(gulp.dest('./build/client'))
+    .pipe(jshint.extract('auto'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('client_css', function () {
+  return gulp.src(config.client_css)
+    .pipe(gulp.dest('./build/client'))
+    .pipe(browser_sync.reload({ stream: true }));
+});
+
 (function () {
-
-  gulp.task('client_html', function () {
-    gulp.src(config.client_html)
-      .pipe(gulp.dest('./build/client'));
-  });
-
-  gulp.task('client_css', function () {
-    gulp.src(config.client_css)
-      .pipe(gulp.dest('./build/client'))
-      .pipe(browser_sync.reload({ stream: true }));
-  });
-
   var customOpts = {
     entries: ['./src/client/main.js'],
     debug: true
@@ -102,7 +110,7 @@ gulp.task('js', function () {
   });
   watched.on('log', gutil.log); // output build logs to terminal
   gulp.task('client_js_watch', function () {
-    dobundle(watched);
+    return dobundle(watched);
   });
 
   var nonwatched = browserify(opts);
@@ -110,22 +118,23 @@ gulp.task('js', function () {
   nonwatched.transform(babelify);
   nonwatched.on('log', gutil.log); // output build logs to terminal
   gulp.task('client_js', function () {
-    dobundle(nonwatched);
+    return dobundle(nonwatched);
   });
 }());
 
 //////////////////////////////////////////////////////////////////////////
 // Combined tasks
 
-gulp.task('build', ['js', 'ts', 'client_html', 'client_css', 'client_js']);
+gulp.task('build', ['jshint', 'js', 'ts', 'client_html', 'client_css', 'client_js']);
 
 gulp.task('bs-reload', function () {
   browser_sync.reload();
 });
 
-gulp.task('watch', ['js', 'ts', 'client_html', 'client_css', 'client_js_watch'], function() {
+gulp.task('watch', ['jshint', 'js', 'ts', 'client_html', 'client_css', 'client_js_watch'], function() {
   gulp.watch(config.ts_files, ['ts']);
   gulp.watch(config.js_files, ['js']);
+  gulp.watch(config.all_js_files, ['jshint']);
   gulp.watch(config.client_html, ['client_html', 'bs-reload']);
   gulp.watch(config.client_css, ['client_css']);
 });
